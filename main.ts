40f703c308134994b75c4de3ecf3b84dec003f6d
DefenestratedBirb. <lanchuzheng@gmail.com>
2025-04-24 12:19:30 +1000

    Update

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-import { serve } from "https://deno.land/std@0.116.0/http/server.ts"
-import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts"
+import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
+import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
-const serveFiles = (req: Request) => staticFiles ('public') ({ 
-    request: req, 
-    respondWith: (r: Response) => r 
-})
+const serveFiles = (req: Request) =>
+  staticFiles("public")({
+    request: req,
+    respondWith: (r: Response) => r,
+  });
 
-serve (req => serveFiles (req), { addr: ':80' })
+serve((req) => serveFiles(req), { addr: ":80" });
 
-console.log (`Listening on http://localhost:80/`)
+console.log(`Listening on http://localhost:80/`);
